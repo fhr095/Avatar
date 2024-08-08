98dc630f5c93cbee20f2ab664a6d484902c0febe
fhr095 <143469663+fhr095@users.noreply.github.com>
2024-08-08 12:44:45 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import AvatarScene from './components/AvatarScene';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Avatar Animation Demo</h1>
+      </header>
+      <AvatarScene />
+    </div>
+  );
+}
+
+export default App;
